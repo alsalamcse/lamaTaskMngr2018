e6325c5659eadd4d4c15fbab97fab7c251622a80
user <muhammed.awad@outlook.com>
2018-10-24 08:56:14 +0300

    addTaskActivity MyTask

--- app/src/main/java/com/sholi/lama/lamataskmngr2018/taskfragments/AddTaskActivity.java
+++ app/src/main/java/com/sholi/lama/lamataskmngr2018/taskfragments/AddTaskActivity.java
@@ -1,15 +1,47 @@
 package com.sholi.lama.lamataskmngr2018.taskfragments;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 import com.sholi.lama.lamataskmngr2018.R;
 
 public class AddTaskActivity extends AppCompatActivity {
+private EditText etText,etTitle,etDueDate;
+private SeekBar skbrImportnat,skbrNercessary;
+private Button btnSave,btnDastePicker;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        etTitle=findViewById(R.id.etTitle);
+        etText=findViewById(R.id.etText);
+        etDueDate=findViewById(R.id.etDueDate);
+        skbrImportnat=findViewById(R.id.skbrImportant);
+        skbrNercessary=findViewById(R.id.skbrNercessary);
+        btnSave=findViewById(R.id.btnSave);
+        btnDastePicker=findViewById(R.id.btnDatePicker);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnDastePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
